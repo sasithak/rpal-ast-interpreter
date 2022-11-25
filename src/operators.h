067fc1308f310a6cbe9c6f50c6d0fcab398df86e
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-11-25 17:08:10 +0530

    Added documentation comments for 'operators.h'

--- src/operators.h
+++ src/operators.h
@@ -3,7 +3,18 @@
 
 #include <iostream>
 
+/**
+ * @brief Identify whether a unary operator by name
+ * @param op The name of the operator
+ * @return true if the name refers to a unary operator, false otherwise
+ */
 bool isUnOp(std::string op);
+
+/**
+ * @brief Identify whether a binary operator by name
+ * @param op The name of the operator
+ * @return true if the name refers to a binary operator, false otherwise
+ */
 bool isBinOp(std::string op);
 
 #endif // OPERATORS_H
